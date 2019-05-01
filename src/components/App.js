a19002a51582becfacd72fb9b74cbb36d3eab457
Zach G <zabros20@gmail.com>
2019-05-01 08:07:13 -0600

    First Commit, created react environment

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
+}
+
+
+export default App
