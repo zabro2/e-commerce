8cac54aeb00795dfd96a7aa03f54f07b98e7c79e
Zach G <zabros20@gmail.com>
2019-05-02 07:55:38 -0600

    Commit 3, created api calls

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    state = {
+        itemList: null
+    }
+    render() {
+        return (
             <div>
                 App
             </div>
         )
     }
+
+    componentDidMount() {
+        fetch('https://my-json-server.typicode.com/tdmichaelis/json-api/products')
+            .then(result => result.json())
+            .then(items => {
+                this.setState({
+                    itemList: items
+                })
+            })
+            setTimeout((() => console.log(this.state.itemList)), 1000)
+    }
+
 }
 
 
---
+++ src/components/Items.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Items = (props) => {
+    return(
+        <div>
+            <div>{}</div>{/*picture*/}
+            <div>{}</div>{/*price*/}
+            <div></div> {/*title*/}
+        </div>
+    )
+}
